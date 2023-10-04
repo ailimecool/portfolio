68dcd8ed052f0bb75bbb6bfe86e63cc02ab647b3
Emilia Fredriksson <frem22pu@student.ju.se>
2023-10-04 18:39:02 +0200

    spelling truoble

--- portfolio.js
+++ portfolio.js
@@ -36,7 +36,7 @@ app.get("/contact", function (req, res) {
 });
 
 const sqlite3 = require("sqlite3");
-const db = new new sqlite3.Database("database-portfolio.db")();
+const db = new sqlite3.Database("database-portfolio.db")();
 
 app.listen(port, () => {
   console.log("Server running and listening on port 2003...");
